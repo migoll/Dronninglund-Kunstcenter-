83fdb430927a0e5b99af3da6099aa4730a4a4e41
Gabbana <chris.lund02@gmail.com>
2025-01-07 09:21:57 +0100

    fixed router view

--- dronninglund-kunstcenter-website/app.vue
+++ dronninglund-kunstcenter-website/app.vue
@@ -1,12 +1,13 @@
 <template>
   <div>
     <TheHeader />
-    <NuxtPage />
+    <RouterView />
     <TheFooter />
   </div>
 </template>
 
 <script setup>
+import { RouterView } from "vue-router";
 import TheFooter from "./components/the-footer.vue";
 import TheHeader from "./components/the-header.vue";
 </script>
